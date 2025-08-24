744e41987f54c3d001a0ac6fcba0c65cc1e0deaf
Ryu Nakamura <dahlia1209@gmail.com>
2025-08-24 21:25:35 +0900

    20250824 2125 OGPタグの設定 #3

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,60 +1,80 @@
-import { defineConfig,PageData ,loadEnv} from 'vitepress'
-import {load} from '../src/data/contents.data'
+import { defineConfig, PageData, loadEnv } from 'vitepress'
+import { load } from '../src/data/contents.data'
 
 export default defineConfig({
-  title: "Ryu Nakamura",
-  description: "Ryu's web site",
+  title: 'Ryu Nakamura',
+  description: 'Webサイト開発やデジタルコンテンツの紹介・販売を行っています。最新のYouTubeトレンドやXトレンド情報も配信中。',
   head: [
     ['link', { rel: 'icon', href: '/home.svg' }],
+    ['meta', { property: 'og:title', content: 'Webサイト・デジタルコンテンツ制作 | Ryu Nakamura' }],
+    ['meta',{property: 'og:description',content:'Webサイト開発やデジタルコンテンツの紹介・販売を行っています。最新のYouTubeトレンドやXトレンド情報も配信中。',},],
+    ['meta',{property: 'og:image',content: 'https://nakamurast20250505.blob.core.windows.net/root/content-image/ogp-logo.png',},],
+    ['meta', { property: 'og:type', content: 'website' }],
+    ['meta', { property: 'og:url', content: 'https://www.ryu-nakamura.com' }],
+    ['meta', { property: 'twitter:card', content: 'summary' }],
+    ['meta',{ property: 'twitter:title', content: 'Webサイト・デジタルコンテンツ制作 | Ryu Nakamura' }],
+    ['meta',{property: 'twitter:description',content:'Webサイト開発やデジタルコンテンツの紹介・販売を行っています。最新のYouTubeトレンドやXトレンド情報も配信中。',},],
+    ['meta',{property: 'twitter:image',content: 'https://nakamurast20250505.blob.core.windows.net/root/content-image/ogp-logo.png',},],
+    ['meta', { property: 'twitter:site', content: '@RyuNakamura' }],
+    ['meta', { property: 'twitter:creator', content: '@RyuNakamura' }],
   ],
-  
+
   async transformPageData(pageData, { siteConfig }) {
     //サブ関数
-    const updateContentsTitle=async (pageData:PageData)=>{
+    const updateContentsMeta = async (pageData: PageData) => {
       if (pageData.relativePath.startsWith('contents/')) {
-        const filename = pageData.relativePath.split('/')[1]; 
-        const titleNo = filename.split('.')[0]; 
-        const data=await load()
-        pageData.title = data.contents.filter(x=>x.title_no.toString()==titleNo)[0].title;
+        pageData.frontmatter.head ??= []
+        const filename = pageData.relativePath.split('/')[1]
+        const titleNo = filename.split('.')[0]
+        const data = await load()
+        const content=data.contents.filter((x) => x.title_no.toString() == titleNo)[0]
+        pageData.title = content.title
+        pageData.frontmatter.head.push(['meta', { property: 'og:title', content: `${content.title} | Ryu Nakamura` }])
+        pageData.frontmatter.head.push(['meta',{property: 'og:description',content:content.preview_text}])
+        pageData.frontmatter.head.push(['meta',{property: 'og:image',content: content.image_url}])
+        pageData.frontmatter.head.push(['meta',{ property: 'twitter:title', content: `${content.title} | Ryu Nakamura` }])
+        pageData.frontmatter.head.push(['meta',{property: 'twitter:description',content:content.preview_text}])
+        pageData.frontmatter.head.push(['meta',{property: 'twitter:image',content:content.image_url}])
       }
     }
 
-    const updateHeader=(pageData:PageData)=>{
+
+    const updateHeader = (pageData: PageData) => {
       pageData.frontmatter.head ??= []
       pageData.frontmatter.head.push([
         'script',
         {
           async: '',
-          src:'https://www.googletagmanager.com/gtag/js?id=G-63Z9Z6L98S'
-        }
+          src: 'https://www.googletagmanager.com/gtag/js?id=G-63Z9Z6L98S',
+        },
       ])
       pageData.frontmatter.head.push([
         'script',
-        {
-       },
+        {},
         `window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
 
-        gtag('config', 'G-63Z9Z6L98S');`
+        gtag('config', 'G-63Z9Z6L98S');`,
       ])
-      }
+      
+    }
 
     //メイン処理
-    await updateContentsTitle(pageData)
-    if(process.env.NODE_ENV=='production')updateHeader(pageData)
+    await updateContentsMeta(pageData)
+    if (process.env.NODE_ENV == 'production') updateHeader(pageData)
     return pageData
   },
   vite: {
     define: (() => {
-      const env = loadEnv(process.env.NODE_ENV ?? "development", `${process.cwd()}/docs`, 'VITE_')
+      const env = loadEnv(process.env.NODE_ENV ?? 'development', `${process.cwd()}/docs`, 'VITE_')
       const defineEnv = {}
-      
-      Object.keys(env).forEach(key => {
+
+      Object.keys(env).forEach((key) => {
         defineEnv[`import.meta.env.${key}`] = JSON.stringify(env[key])
       })
-      
+
       return defineEnv
-    })()
-  }
+    })(),
+  },
 })

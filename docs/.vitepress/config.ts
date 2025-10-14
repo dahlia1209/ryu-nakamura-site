3a82f77016c4f0413b8db0f0244ad61c7d0b5c4a
Ryu Nakamura <dahlia1209@gmail.com>
2025-10-14 20:42:29 +0900

    202251014 2042 SEO対策 #4

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,17 +3,17 @@ import { load } from '../src/data/contents.data'
 
 export default defineConfig({
   title: 'Ryu Nakamura',
-  description: 'Webサイト開発やデジタルコンテンツの紹介・販売を行っています。最新のYouTubeトレンドやXトレンド情報も配信中。',
+  description: '個人開発アプリとデジタルコンテンツを提供。Vue.js/TypeScript製のWebアプリケーション、ブロックチェーンシミュレーター、技術記事を公開。中村システムエンジニアリング事業所。',
   head: [
     ['link', { rel: 'icon', href: '/home.svg' }],
     ['meta', { property: 'og:title', content: 'Webサイト・デジタルコンテンツ制作 | Ryu Nakamura' }],
-    ['meta',{property: 'og:description',content:'Webサイト開発やデジタルコンテンツの紹介・販売を行っています。最新のYouTubeトレンドやXトレンド情報も配信中。',},],
+    ['meta',{property: 'og:description',content:'個人開発アプリとデジタルコンテンツを提供。Vue.js/TypeScript製のWebアプリケーション、ブロックチェーンシミュレーター、技術記事を公開。中村システムエンジニアリング事業所。',},],
     ['meta',{property: 'og:image',content: 'https://nakamurast20250505.blob.core.windows.net/root/content-image/ogp-logo.png',},],
     ['meta', { property: 'og:type', content: 'website' }],
     ['meta', { property: 'og:url', content: 'https://www.ryu-nakamura.com' }],
     ['meta', { property: 'twitter:card', content: 'summary' }],
     ['meta',{ property: 'twitter:title', content: 'Webサイト・デジタルコンテンツ制作 | Ryu Nakamura' }],
-    ['meta',{property: 'twitter:description',content:'Webサイト開発やデジタルコンテンツの紹介・販売を行っています。最新のYouTubeトレンドやXトレンド情報も配信中。',},],
+    ['meta',{property: 'twitter:description',content:'個人開発アプリとデジタルコンテンツを提供。Vue.js/TypeScript製のWebアプリケーション、ブロックチェーンシミュレーター、技術記事を公開。中村システムエンジニアリング事業所。',},],
     ['meta',{property: 'twitter:image',content: 'https://nakamurast20250505.blob.core.windows.net/root/content-image/ogp-logo.png',},],
     ['meta', { property: 'twitter:site', content: '@RyuNakamura' }],
     ['meta', { property: 'twitter:creator', content: '@RyuNakamura' }],
